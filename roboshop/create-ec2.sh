3b5bc7e55a43a93c6b73cd154dbd796e2eadc680
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-06-10 18:00:40 +0530

    wrapper file

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -4,7 +4,7 @@
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId'|sed -e 's/"//g')
 
-echo "AMI ID is $AMI_ID"
+echo -e "AMI ID is to launch the EC2 is \e[32m $AMI_ID \e[0m"
 
 
 aws ec2 run-instances --image-id $(AMI_ID) --instance-type t2.micro
