b6502d019b32abef0f756064395bf46f216fd2ce
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-06-10 18:04:34 +0530

    wtrewt

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -7,4 +7,5 @@ AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-Cen
 echo -e "AMI ID is to launch the EC2 is \e[32m $AMI_ID \e[0m"
 
 
+echo -e  "*** Launching server **** "
 aws ec2 run-instances --image-id $(AMI_ID) --instance-type t2.micro
