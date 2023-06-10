710199298b5c7c66a29604c557e8d9698c153da0
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-06-10 17:53:34 +0530

    wrapper file

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -2,7 +2,7 @@
 
 # AMI_ID="ami-0c1d144c8fdd8d690"
 
-AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId')
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId'|sed -e 's/"//g')
 
 echo "AMI ID is $AMI_ID"
 
