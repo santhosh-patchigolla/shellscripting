ab09d5f5459129d8909cf454eb4bda8898aa228b
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-06-11 21:27:38 +0530

    Changes

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -8,6 +8,5 @@ SG_ID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=allow-
 echo -e "AMI ID is to launch the EC2 is \e[32m $AMI_ID \e[0m"
 echo -e "Security Group ID used to launch the EC2 is \e[35m $SG_ID \e[0m"
 
-
 echo -e  "*** Launching server **** "
 aws ec2 run-instances --image-id ami-0c1d144c8fdd8d690 --instance-type t2.micro
