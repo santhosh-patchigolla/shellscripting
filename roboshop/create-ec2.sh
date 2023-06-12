7914b8473ea795440815c9312ff23fa7e54face7
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-06-12 15:35:37 +0530

    Changes

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -2,6 +2,8 @@
 
 # AMI_ID="ami-0c1d144c8fdd8d690"
 
+COMPONENT=$1
+
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId'|sed -e 's/"//g')
 SG_ID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=allow-all-sg-b54 | jq '.SecurityGroups[].GroupID' | sed -e 's/"//g')
 
@@ -10,5 +12,5 @@ echo -e "Security Group ID used to launch the EC2 is \e[35m $SG_ID \e[0m"
 
 echo -e  "*** Launching server **** "
 
-aws ec2 run-instances --image-id ami-0c1d144c8fdd8d690 --instance-type t3.micro --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=payment}]' | jq .
+aws ec2 run-instances --image-id ami-0c1d144c8fdd8d690 --instance-type t3.micro --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]' | jq .
         
