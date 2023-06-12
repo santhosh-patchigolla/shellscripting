bd8184b8fe44faf2bd50f28b1cb7aadb57dda181
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-06-12 15:51:19 +0530

    Changes

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -12,5 +12,5 @@ echo -e "Security Group ID used to launch the EC2 is \e[35m $SG_ID \e[0m"
 
 echo -e  "*** Launching server **** "
 
-aws ec2 run-instances --image-id ami-0c1d144c8fdd8d690 --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq .
+aws ec2 run-instances --image-id ami-0c1d144c8fdd8d690 --instance-type t3.micro --security-group-ids sg-08212398e8c9c44b6 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq .
         
