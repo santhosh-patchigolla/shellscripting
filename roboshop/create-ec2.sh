e9ec6d71479124ee56e4f18ed735cce6db5e1d7a
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-06-12 18:23:34 +0530

    Changes

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -3,6 +3,7 @@
 # AMI_ID="ami-0c1d144c8fdd8d690"
 
 COMPONENT=$1
+ENV=$2
 HOSTEDZONEID="Z069258112MM0I3IYRYGR"
 
 if [ -z "$1" ] ; then
@@ -20,13 +21,13 @@ create_ec2() {
         echo -e "Security Group ID used to launch the EC2 is \e[35m $SG_ID \e[0m"
         echo -e  "*** Launching server **** "
 
-        IPADDRESS=$(aws ec2 run-instances --image-id ami-0c1d144c8fdd8d690 --instance-type t3.micro --security-group-ids sg-08212398e8c9c44b6 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+        IPADDRESS=$(aws ec2 run-instances --image-id ami-0c1d144c8fdd8d690 --instance-type t3.micro --security-group-ids sg-08212398e8c9c44b6 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT-$ENV}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
-        echo -e  "*** Launching $COMPONENT server **** "
-        echo -e "Private IP Address of $COMPONENT is $IPADDRESS"  
+        echo -e  "*** Launching $COMPONENT-$ENV server **** "
+        echo -e "Private IP Address of $COMPONENT-$ENV is $IPADDRESS"  
         echo -e "\e[36m **** Creating DNS Record for the $COMPONENT :  **** \e[0m"
 
-        sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${IPADDRESS}/" route53.json > /tmp/record.json
+        sed -e "s/COMPONENT/${COMPONENT}-$ENV/" -e "s/IPADDRESS/${IPADDRESS}/" route53.json > /tmp/record.json
         aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/record.json
 
         echo -e "\e[36m **** Creating DNS Record for the $COMPONENT :  **** \e[0m \n\n"
